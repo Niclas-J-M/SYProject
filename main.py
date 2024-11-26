d8c1cfdaa284d3d3834aeb6ba82dab3bf36ea270
unknown <muellerhof.n@googlemail.com>
2024-11-26 18:43:11 +0100

    added image and fixed a small bug

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from env.ScotlandYard_env import ScotlandYardEnv
 import numpy as np
 
 def main():
-    print("hello")
     register(
         id="ScotlandYard-v0",
         entry_point="env.ScotlandYard_env:ScotlandYardEnv",
@@ -16,27 +15,29 @@ def main():
 
     while not done:
         # Mister X action
+        mister_x_pos = env.unwrapped.mister_x_position
         valid_actions = env.unwrapped.get_valid_actions(0)
         action = valid_actions[np.random.randint(len(valid_actions))]  # Random valid action
-        print(env.unwrapped.mister_x_position)
-        print(action)
-        print(*action)
         obs, reward, terminated, truncated, info = env.step((0, *action))
         done = terminated or truncated
-        print(f"Mister X took action: {action}, Reward: {reward}")
-        env.render()
+        print(f"Mister X took action: {mister_x_pos} to {action[0]} using {action[1]}, Reward: {reward}")
+        
 
         if done:
             break
 
         # Detectives' actions
-        for detective_id in range(1, 6):
+        for detective_id in range(1, 5):
+            detective_pos = env.unwrapped.detectives_positions[detective_id]
+            print("det pos", detective_pos)
             valid_actions = env.unwrapped.get_valid_actions(detective_id)
+            print("valid actions", valid_actions)
             if valid_actions:  # Ensure there are valid actions
                 action = valid_actions[np.random.randint(len(valid_actions))]  # Random valid action
-                obs, reward, done, info = env.step((detective_id, *action))
-                print(f"Detective {detective_id} took action: {action}, Reward: {reward}")
-                env.render()
+                obs, reward, terminated, truncated, info = env.step((detective_id, *action))
+                done = terminated or truncated
+                print(f"Detective {detective_id} took action: {detective_pos} to {action[0]} using {action[1]}, Reward: {reward}")
+                
 
             if done:
                 break
